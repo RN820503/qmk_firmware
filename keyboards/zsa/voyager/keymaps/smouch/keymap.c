0908fb79a42f885ec311f63f9725566d485ca350
RN820503 <roderick.smith@me.com>
2025-07-17 13:05:05 +1000

    Numrow coverted to Utilities

--- keyboards/zsa/voyager/keymaps/smouch/keymap.c
+++ keyboards/zsa/voyager/keymaps/smouch/keymap.c
@@ -4,11 +4,12 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _DEF,
-    _NUM,
-    _SYM,
-    _NAV,
-    _GAM,
+    DEF,
+    NUM,
+    SYM,
+    NAV,
+    WIN,
+    GAM,
 };
 
 enum custom_keycodes {
@@ -24,60 +25,60 @@ enum custom_keycodes {
 */
 
 /*  Hands Down Promethium (canonical)
-    ⎋   3   2@  1$   0#  4           7   6   5   9   8  Vol
-    ⇥   v   w   g2   m   j           =_  .:  '2  -+  /  Vol
+    `      cut all  cpy              ⌘←  ←   →   ⌘→  ⌘⇥ mply
+    ⇥   v   w   g2   m   j           =_  .:  '2  -+  /  ⎋
     z   s⌃  n⌥  t3   h⌘  k           ,;  a⌘  e1  i⌥  c⌃ q
-    `   f   p   d    l   x      	 `   u   o   y   b  \
+    tg  f   p   d    l   x      	 ;   u   o   y   b  tg
                          r⇧  ⌫   ⏎   ␣⇧
-  */
+*/
 
 // Num row
-#define LN5 KC_ESC
-#define LN4 KC_3
-#define LN3 KC_2
-#define LN2 KC_1
-#define LN1 KC_0
-#define LN0 KC_4
+#define LN5 KC_GRV
+#define LN4 KC_NO
+#define LN3 G(KC_X)
+#define LN2 G(KC_A)
+#define LN1 G(KC_C)
+#define LN0 KC_NO
 
-#define RN0 KC_7
-#define RN1 KC_6
-#define RN2 KC_5
-#define RN3 KC_9
-#define RN4 KC_8
-#define RN5 KC_VOLU
+#define RN0 G(KC_LEFT)
+#define RN1 KC_LEFT
+#define RN2 KC_RGHT
+#define RN3 G(KC_RGHT)
+#define RN4 G(KC_TAB)
+#define RN5 KC_MPLY
 
 // top row
 #define LT5 KC_TAB
 #define LT4 KC_V
 #define LT3 KC_W
-#define LT2 LT(_SYM, KC_G)
+#define LT2 LT(SYM, KC_G)
 #define LT1 KC_M
 #define LT0 KC_J
 
 #define RT0 KC_EQL
 #define RT1 KC_DOT
-#define RT2 LT(_SYM, KC_QUOT)
+#define RT2 LT(SYM, KC_QUOT)
 #define RT3 KC_MINS
 #define RT4 KC_SLSH
-#define RT5 KC_VOLD
+#define RT5 KC_ESC
 
 // middle row
 #define LM5 KC_Z
 #define LM4 LCTL_T(KC_S)
 #define LM3 LALT_T(KC_N)
-#define LM2 LT(_NAV, KC_T)
+#define LM2 LT(NAV, KC_T)
 #define LM1 LGUI_T(KC_H)
 #define LM0 KC_K
 
 #define RM0 KC_COMM
 #define RM1 RGUI_T(KC_A)
-#define RM2 LT(_NUM, KC_E)
+#define RM2 LT(NUM, KC_E)
 #define RM3 RALT_T(KC_I)
 #define RM4 RCTL_T(KC_C)
 #define RM5 KC_Q
 
 // bottom row
-#define LB5 KC_GRV
+#define LB5 KC_NO
 #define LB4 KC_F
 #define LB3 KC_P
 #define LB2 KC_D
@@ -87,9 +88,9 @@ enum custom_keycodes {
 #define RB0 KC_SCLN
 #define RB1 KC_U
 #define RB2 KC_O
-#define RB3 KC_Y
+#define RB3 LT(WIN, KC_Y)
 #define RB4 KC_B
-#define RB5 TG(_GAM)
+#define RB5 TG(GAM)
 
 // thumb row
 #define LH1 LSFT_T(KC_R)
@@ -107,14 +108,14 @@ static uint16_t keyhold_timer; // for handling Qu combo
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /*  Hands Down Promethium
-    ⎋   3   2@  1$   0#  4           7   6   5   9   8  Vol
-    ⇥   v   w   g2   m   j           =_  .:  '2  -+  /  Vol
+  /*  Hands Down Promethium (canonical)
+    `      cut all  cpy              ⌘←  ←   →   ⌘→  ⌘⇥ mply
+    ⇥   v   w   g2   m   j           =_  .:  '2  -+  /  ⎋
     z   s⌃  n⌥  t3   h⌘  k           ,;  a⌘  e1  i⌥  c⌃ q
-    `   f   p   d    l   x      	 ;   u   o   y   b  \
+    tg  f   p   d    l   x      	 ;   u   o   y   b  tg
                          r⇧  ⌫   ⏎   ␣⇧
-  */
-  [_DEF] = LAYOUT_voyager(
+*/
+  [DEF] = LAYOUT_voyager(
     LN5, LN4, LN3, LN2, LN1, LN0,           RN0, RN1, RN2, RN3, RN4, RN5,
     LT5, LT4, LT3, LT2, LT1, LT0,           RT0, RT1, RT2, RT3, RT4, RT5,
     LM5, LM4, LM3, LM2, LM1, LM0,           RM0, RM1, RM2, RM3, RM4, RM5,
@@ -124,26 +125,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  /*	BEAKL-19 Numpad
 		⇥	+	=	*
-    ⇥   -   5   2   3   :
-    lk  7   .   1   0   4       	,   ⌘  (mo)  ⌥	 ⌃
-        /   6   9   8   ,
-    	                ␣   ⌫   ⏎   ␣⇧
+    ⇥   -   5   2   3   /
+        7   .   1   0   4       	,   ⌘  (mo)  ⌥	 ⌃
+        :   6   9   8   ,
+    	                ␣   ⌫   ⏎   lk
   */
-  [_NUM] = LAYOUT_voyager(
+  [NUM] = LAYOUT_voyager(
     _______, KC_TAB,  KC_PLUS, KC_EQL, KC_ASTR,  ___x___,                   _______, _______, _______, _______, _______, _______,
-    _______, KC_MINS, KC_5,    KC_2,    KC_3,    KC_COLN,                   _______, _______, _______, _______, _______, _______,
-    QK_LLCK, KC_7,    KC_DOT,  KC_1,    KC_0,    KC_4,                      _______, KC_RGUI, _______, KC_RALT, KC_RCTL, _______,
-    _______, KC_SLSH, KC_6,    KC_9,    KC_8,    KC_COMM,                   _______, ___x___, ___x___, ___x___, ___x___, _______,
-                                                 KC_SPC,  _______, _______, _______
+    _______, KC_MINS, KC_5,    KC_2,    KC_3,    KC_SLSH,                   _______, _______, _______, _______, _______, _______,
+    _______, KC_7,    KC_DOT,  KC_1,    KC_0,    KC_4,                      _______, KC_RGUI, _______, KC_RALT, KC_RCTL, _______,
+    _______, KC_COLN, KC_6,    KC_9,    KC_8,    KC_COMM,                   _______, ___x___, ___x___, ___x___, ___x___, _______,
+                                                 KC_SPC,  _______, _______, QK_LLCK
   ),
 
-  /* Getreur Symbols
+  /* Getreuer Symbols
     *
     *	`	<   >	-	|		    ^  	{   }   $
     *   !   *   /   =   &          	#   (   )   ;   "
     *   ~   +   [	]   %		    @ 	:   ,   .	'
   */
-  [_SYM] = LAYOUT_voyager(
+  [SYM] = LAYOUT_voyager(
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, KC_GRV , KC_LABK, KC_RABK, KC_MINS, KC_PIPE,                   KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR , KC_BSLS, _______,
     QK_LLCK, KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL , KC_AMPR,                   KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO, QK_LLCK,
@@ -167,17 +168,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
 
   /*
-    	                        Sclp  Scap                     vol+    home    up	end
-    	⌃      ⌥       (mo)     ⌘     SelAll       		       vol-    left    dn	rght	del  lk
-    	undo   cpy             pst    redo                     mute    ←wrd    wrd→ ←line→  rgb
-                                        ⇧                      dsk-    dsk+
+    	                        Sclp                       Pgup    home    up	end
+    	⌃      ⌥       (mo)     ⌘            		       Pgdn    left    dn	rght	del
+    	                        Scap                       undo    ←wrd    wrd→ ←line→
+                                        ⇧                  Lock
   */
-  [_NAV] = LAYOUT_voyager(
-    _______, _______, _______, _______, _______,       _______,                        _______, _______,    _______, _______,    _______, _______,
-    _______, ___x___, ___x___, _______, C(S(G(KC_4))), LSG(KC_4),                      KC_VOLU, G(KC_LEFT), KC_UP,   G(KC_RGHT), ___x___, _______,
-    ___x___, KC_LCTL, KC_LALT, _______, KC_LGUI,       G(KC_A),                        KC_VOLD, KC_LEFT,    KC_DOWN, KC_RGHT,    KC_DEL,  QK_LLCK,
-    ___x___, G(KC_Z), G(KC_C), G(KC_X), G(KC_V),       LSG(KC_Z),                      KC_MUTE, SELWBAK,    SELWORD, SELLINE,    LUMINO,  _______,
-                                                       KC_LSFT,   _______, C(KC_LEFT), C(KC_RGHT)
+  [NAV] = LAYOUT_voyager(
+    _______, _______, _______, _______, _______,       _______,                   _______, _______,    _______, _______,    _______, _______,
+    _______, ___x___, ___x___, _______, C(S(G(KC_4))), ___x___,                   KC_PGUP, G(KC_LEFT), KC_UP,   G(KC_RGHT), ___x___, _______,
+    ___x___, KC_LCTL, KC_LALT, _______, KC_LGUI,       ___x___,                   KC_PGDN, KC_LEFT,    KC_DOWN, KC_RGHT,    KC_DEL,  _______,
+    ___x___, ___x___, ___x___, ___x___, LSG(KC_4),     ___x___,                   G(KC_Z), SELWBAK,    SELWORD, SELLINE,    LUMINO,  _______,
+                                                       KC_LSFT, _______, _______, QK_LLCK
+  ),
+
+  /*	Windows Management
+
+                mute vol↓ vol↑
+                     dsk← dsk→          	⌘  ⇧  ⌥	 ⌃
+                                                 (mo)
+    	                        mply    lk
+  */
+  [WIN] = LAYOUT_voyager(
+    ___x___, ___x___, ___x___, ___x___,    ___x___,    ___x___,                   _______, _______, _______, _______, _______, _______,
+    ___x___, ___x___, KC_MUTE, KC_VOLD,    KC_VOLU,    ___x___,                   ___x___, ___x___, ___x___, ___x___, ___x___, _______,
+    ___x___, ___x___, ___x___, C(KC_LEFT), C(KC_RGHT), ___x___,                   ___x___, KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL, ___x___,
+    ___x___, ___x___, ___x___, ___x___,    ___x___,    ___x___,                   ___x___, ___x___, ___x___, _______, ___x___, ___x___,
+                                                       KC_MPLY, ___x___, ___x___, QK_LLCK
   ),
 
   /*	Game
@@ -187,7 +203,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     v   ⌃   F2  F1  F3  F4                           tg
     	                ␣   c   ⏎   ␣⇧
   */
-  [_GAM] = LAYOUT_voyager(
+  [GAM] = LAYOUT_voyager(
     _______, _______, KC_2,  KC_1,  KC_3,  KC_4,                         _______, _______, _______, _______, _______, _______,
     _______, KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,                         _______, _______, _______, _______, _______, _______,
     KC_Z,    KC_LSFT, KC_A,  KC_S,  KC_D,  KC_F,                         _______, KC_RGUI, _______, KC_RALT, KC_RCTL, _______,
